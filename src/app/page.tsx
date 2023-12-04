f4806f10331bde2bcb1d8b1094bf0e125c760d15
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2023-12-04 22:41:16 +0700

    feat: login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,72 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="h-[30vw] bg-[#6930B7] pl-[5%] flex justify-between pt-24">
+        <div className="flex flex-col pb-20 w- justify-between h-full">
+          <h1 className="font-bold text-3xl md:text-8xl text-white">
+            Smart Dorm
+          </h1>
+          <h3 className="text-white text-xl md:text-4xl">
+            Ketika kenyamanan bertemu konektivitas.
+          </h3>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          alt="landing iamge"
+          src="/landing-img.svg"
+          width={1200}
+          height={500}
+          className="w-[52%] relative bottom-0 right-0"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex justify-center mt-6">
+        <Link
+          href="/login"
+          className="bg-[#6930B7] py-4 px-2 text-center font-semibold self-center rounded-lg w-44 text-white mt-6"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Login
+        </Link>
       </div>
-    </main>
-  )
+
+      <div className="bg-[url('/landing-bg.svg')] bg-no-repeat bg-right">
+        <div className="p-20 flex gap-6 items-center bg-[url('/landing-kanan.svg')] bg-left bg-no-repeat">
+          <div className="w-2/3 flex flex-col gap-6">
+            <h1 className="font-semibold text-5xl">Mengapa Smart Dorm?</h1>
+
+            <div className="bg-white p-7 rounded-3xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              eu libero turpis. In nunc purus, cursus eget est a, commodo
+              ultrices lacus. Etiam pharetra mauris at nulla tincidunt, in
+              vulputate quam venenatis. Nulla vitae dolor vitae urna hendrerit
+              tempus quis fermentum est. Etiam fermentum molestie nisl vitae
+              laoreet. Fusce lectus lectus, faucibus in venenatis ac, pharetra
+              vel nisl. Phasellus ullamcorper nibh eget metus aliquam convallis.
+              Vestibulum magna mauris, venenatis imperdiet scelerisque in,
+              ultrices vitae urna.
+              <br /> <br />
+              Nampharetra laoreet dui. Vivamus in diam fringilla, auctor sapien
+              at, tristique purus. Vestibulum a est sollicitudin, vestibulum dui
+              ut, aliquam tortor. Donec vehicula sit amet eros nec condimentum.
+              Ut efficitur massa eget ipsum pellentesque, non cursus odio porta.
+              Phasellus quis rhoncus purus, eget hendrerit odio. Nullam bibendum
+              enim eu felis sodales ultricies. Nullam libero sapien, dignissim
+              et odio auctor, molestie fermentum lectus. Morbi nulla diam,
+              sagittis sed dolor ut, dictum consequat erat.
+            </div>
+          </div>
+
+          <Image
+            src="/dummy-landing.svg"
+            width={327}
+            height={327}
+            alt="dummy"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
