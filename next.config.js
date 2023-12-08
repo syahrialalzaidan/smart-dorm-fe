d2110266c1576d6886a5a1ed745405e788373a98
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-12-08 18:57:08 +0700

    Fix: dotenv configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// webpack
+const webpack = require('webpack')
+
+const { parsed: myEnv } = require('dotenv').config({
+    path:'./.env',
+})
+
+const nextConfig = {
+    webpack(config) {
+        config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+        return config
+    }
+}
 
 module.exports = nextConfig
